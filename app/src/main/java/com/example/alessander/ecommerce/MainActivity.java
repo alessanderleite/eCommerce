1de6bc412dd1acd4c9dbe3454578a3bf43162ed2
alessanderleite <alessander.logistica@gmail.com>
2018-12-01 16:38:34 -0200

    set main activity layout

--- app/src/main/java/com/example/alessander/ecommerce/MainActivity.java
+++ app/src/main/java/com/example/alessander/ecommerce/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.alessander.ecommerce;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button joinNowButton, loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        joinNowButton = (Button) findViewById(R.id.main_join_now_btn);
+        loginButton = (Button) findViewById(R.id.main_login_btn);
     }
 }
