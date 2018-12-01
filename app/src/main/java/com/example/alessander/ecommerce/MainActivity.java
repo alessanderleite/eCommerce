79f97369ff37ee6b5a79972219c4021c8702fdc3
alessanderleite <alessander.logistica@gmail.com>
2018-12-01 21:11:26 -0200

    call intent in the mainActivity

--- app/src/main/java/com/example/alessander/ecommerce/MainActivity.java
+++ app/src/main/java/com/example/alessander/ecommerce/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.alessander.ecommerce;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,5 +17,14 @@ public class MainActivity extends AppCompatActivity {
 
         joinNowButton = (Button) findViewById(R.id.main_join_now_btn);
         loginButton = (Button) findViewById(R.id.main_login_btn);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
