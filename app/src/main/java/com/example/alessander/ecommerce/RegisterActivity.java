846e9bc9f732dae32ab6cc487e171ab5fe01a25f
alessanderleite <alessander.logistica@gmail.com>
2018-12-02 11:47:11 -0200

    instantiate the Button and the edit texts into RegisterActivity class

--- app/src/main/java/com/example/alessander/ecommerce/RegisterActivity.java
+++ app/src/main/java/com/example/alessander/ecommerce/RegisterActivity.java
@@ -2,12 +2,22 @@ package com.example.alessander.ecommerce;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private Button createAccountButton;
+    private EditText inputName, inputPhoneNumber, inputPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        createAccountButton = (Button) findViewById(R.id.register_btn);
+        inputName = (EditText) findViewById(R.id.register_username_input);
+        inputPassword = (EditText) findViewById(R.id.register_password_input);
+        inputPhoneNumber = (EditText) findViewById(R.id.register_phone_number_input);
     }
 }
