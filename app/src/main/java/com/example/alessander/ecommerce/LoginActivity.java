9d8e273c52a8fcf954726735ae4ecd8f1a3cc439
alessanderleite <alessander.logistica@gmail.com>
2018-12-03 07:32:24 -0200

    create a new method called allowAccessToAccount

--- app/src/main/java/com/example/alessander/ecommerce/LoginActivity.java
+++ app/src/main/java/com/example/alessander/ecommerce/LoginActivity.java
@@ -1,13 +1,97 @@
 package com.example.alessander.ecommerce;
 
+import android.app.ProgressDialog;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText inputPhoneNumber, inputPassword;
+    private Button loginButton;
+    private ProgressDialog loadingBar;
+
+    private String parentDbName = "Users";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginButton = (Button) findViewById(R.id.login_btn);
+        inputPassword = (EditText) findViewById(R.id.login_password_input);
+        inputPhoneNumber = (EditText) findViewById(R.id.login_phone_number_input);
+        loadingBar = new ProgressDialog(this);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                loginUser();
+            }
+        });
+    }
+
+    private void loginUser() {
+
+
+        String phone = inputPhoneNumber.getText().toString();
+        String password = inputPassword.getText().toString();
+
+        if (TextUtils.isEmpty(phone)) {
+
+            Toast.makeText(this, "Please write your phone number...", Toast.LENGTH_SHORT).show();
+        }
+        else if (TextUtils.isEmpty(password)) {
+
+            Toast.makeText(this, "Please write your password...", Toast.LENGTH_SHORT).show();
+        }
+        else {
+
+            loadingBar.setTitle("Login Account");
+            loadingBar.setMessage("Please wait, while we are checking the credentials.");
+            loadingBar.setCanceledOnTouchOutside(false);
+            loadingBar.show();
+
+            allowAccessToAccount(phone, password);
+        }
+    }
+
+    private void allowAccessToAccount(final String phone, String password) {
+
+        final DatabaseReference rootRef;
+        rootRef = FirebaseDatabase.getInstance().getReference();
+
+        rootRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                if (dataSnapshot.child(parentDbName).child(phone).exists()) {
+
+
+                }
+                else {
+
+                    Toast.makeText(LoginActivity.this,"Account with this " + phone + " number do not exists.",Toast.LENGTH_SHORT).show();
+                    loadingBar.dismiss();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
