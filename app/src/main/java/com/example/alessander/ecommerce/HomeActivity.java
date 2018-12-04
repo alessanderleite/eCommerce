7b210acd6469013c5fdc12aeab4542cd87149152
alessanderleite <alessander.logistica@gmail.com>
2018-12-03 22:24:12 -0200

    add setOnClickListener and pass an intent

--- app/src/main/java/com/example/alessander/ecommerce/HomeActivity.java
+++ app/src/main/java/com/example/alessander/ecommerce/HomeActivity.java
@@ -1,13 +1,29 @@
 package com.example.alessander.ecommerce;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private Button logoutBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        logoutBtn = (Button) findViewById(R.id.logout_btn);
+
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/alessander/ecommerce/LoginActivity.java
+++ app/src/main/java/com/example/alessander/ecommerce/LoginActivity.java
@@ -93,6 +93,10 @@ public class LoginActivity extends AppCompatActivity {
                             Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
                             startActivity(intent);
                         }
+                        else {
+                            loadingBar.dismiss();
+                            Toast.makeText(LoginActivity.this,"Password is incorrect.", Toast.LENGTH_SHORT).show();
+                        }
                     }
                 }
                 else {
