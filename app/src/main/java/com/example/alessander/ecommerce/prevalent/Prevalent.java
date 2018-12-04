050551a05cfaa79bd66a93042e00930171354085
alessanderleite <alessander.logistica@gmail.com>
2018-12-03 22:45:26 -0200

    add userPhone and userPassword

--- app/src/main/java/com/example/alessander/ecommerce/prevalent/Prevalent.java
+++ app/src/main/java/com/example/alessander/ecommerce/prevalent/Prevalent.java
@@ -5,4 +5,7 @@ import com.example.alessander.ecommerce.model.Users;
 public class Prevalent {
 
     private static Users currentOnlineUser;
+
+    private static final String userPhoneKey = "UserPhone";
+    private static final String userPasswordKey = "UserPassword";
 }
