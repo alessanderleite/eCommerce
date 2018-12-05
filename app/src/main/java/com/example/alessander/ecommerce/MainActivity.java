5aacf2190d064bdae6fcbe56f88c574cef8e05ef
alessanderleite <alessander.logistica@gmail.com>
2018-12-05 07:39:13 -0200

    create the method allowAccess

--- app/src/main/java/com/example/alessander/ecommerce/MainActivity.java
+++ app/src/main/java/com/example/alessander/ecommerce/MainActivity.java
@@ -1,19 +1,29 @@
 package com.example.alessander.ecommerce;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.example.alessander.ecommerce.model.Users;
 import com.example.alessander.ecommerce.prevalent.Prevalent;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import io.paperdb.Paper;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button joinNowButton, loginButton;
+    private ProgressDialog loadingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
 
         joinNowButton = (Button) findViewById(R.id.main_join_now_btn);
         loginButton = (Button) findViewById(R.id.main_login_btn);
+        loadingBar = new ProgressDialog(this);
 
         Paper.init(this);
 
@@ -50,8 +61,57 @@ public class MainActivity extends AppCompatActivity {
 
             if (!TextUtils.isEmpty(userPhoneKey) && !TextUtils.isEmpty(userPasswordKey)) {
 
+                allowAccess(userPhoneKey, userPasswordKey);
 
+                loadingBar.setTitle("Already Logged in");
+                loadingBar.setMessage("Please wait...");
+                loadingBar.setCanceledOnTouchOutside(false);
+                loadingBar.show();
             }
         }
+    }
+
+    private void allowAccess(final String phone, final String password) {
+
+        final DatabaseReference rootRef;
+        rootRef = FirebaseDatabase.getInstance().getReference();
+
+        rootRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                if (dataSnapshot.child("Users").child(phone).exists()) {
+
+                    Users usersData = dataSnapshot.child("Users").child(phone).getValue(Users.class);
+
+                    if (usersData.getPhone().equals(phone)) {
+
+                        if (usersData.getPassword().equals(password)) {
+
+                            Toast.makeText(MainActivity.this,"logged in Successfully...", Toast.LENGTH_SHORT).show();
+                            loadingBar.dismiss();
+
+                            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                            startActivity(intent);
+                        }
+                        else {
+                            loadingBar.dismiss();
+                            Toast.makeText(MainActivity.this,"Password is incorrect.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+                else {
+
+                    Toast.makeText(MainActivity.this,"Account with this " + phone + " number do not exists.",Toast.LENGTH_SHORT).show();
+                    loadingBar.dismiss();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
     }
 }
