88a14ae834c19560badb6522dfa556ca3a08d123
alessanderleite <alessander.logistica@gmail.com>
2018-12-06 21:35:53 -0200

    change toast message

--- app/src/main/java/com/example/alessander/ecommerce/LoginActivity.java
+++ app/src/main/java/com/example/alessander/ecommerce/LoginActivity.java
@@ -129,18 +129,18 @@ public class LoginActivity extends AppCompatActivity {
 
                             if (parentDbName.equals("Admins")) {
 
-                                Toast.makeText(LoginActivity.this,"logged in Successfully...", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(LoginActivity.this,"Welcome Admin, you are logged in Successfully...", Toast.LENGTH_SHORT).show();
                                 loadingBar.dismiss();
 
-                                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                                Intent intent = new Intent(LoginActivity.this, AdminAddNewProductActivity.class);
                                 startActivity(intent);
                             }
                             else if (parentDbName.equals("Users")) {
 
-                                Toast.makeText(LoginActivity.this,"Welcome Admin, your are logged in Successfully...", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(LoginActivity.this,"logged in Successfully...", Toast.LENGTH_SHORT).show();
                                 loadingBar.dismiss();
 
-                                Intent intent = new Intent(LoginActivity.this, AdminAddNewProductActivity.class);
+                                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
                                 startActivity(intent);
                             }
                         }
