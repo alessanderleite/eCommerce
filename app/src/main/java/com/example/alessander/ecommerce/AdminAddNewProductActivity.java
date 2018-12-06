71d9f4e77919774ab4d26bc6b5a36a6efe2390a4
alessanderleite <alessander.logistica@gmail.com>
2018-12-06 08:00:23 -0200

    create new Activity for Admin

---
+++ app/src/main/java/com/example/alessander/ecommerce/AdminAddNewProductActivity.java
@@ -0,0 +1,16 @@
+package com.example.alessander.ecommerce;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class AdminAddNewProductActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_add_new_product);
+
+        Toast.makeText(this,"Welcome Admin...", Toast.LENGTH_SHORT).show();
+    }
+}
