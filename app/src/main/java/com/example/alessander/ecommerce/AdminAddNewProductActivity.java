ce1d8f8bf6a6e09461e90d3b0a34d9a08dc11362
alessanderleite <alessander.logistica@gmail.com>
2018-12-08 12:07:49 -0200

    call findViewById

--- app/src/main/java/com/example/alessander/ecommerce/AdminAddNewProductActivity.java
+++ app/src/main/java/com/example/alessander/ecommerce/AdminAddNewProductActivity.java
@@ -2,11 +2,18 @@ package com.example.alessander.ecommerce;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class AdminAddNewProductActivity extends AppCompatActivity {
 
     private String categoryName;
+    private Button addNewProductButton;
+    private ImageView inputProductImage;
+    private EditText inputProductName, inputProductDescription, inputProductPrice;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +22,10 @@ public class AdminAddNewProductActivity extends AppCompatActivity {
 
         categoryName = getIntent().getExtras().get("category").toString();
 
+        addNewProductButton = (Button) findViewById(R.id.add_new_product);
+        inputProductImage = (ImageView) findViewById(R.id.select_product_image);
+        inputProductName = (EditText) findViewById(R.id.product_name);
+        inputProductDescription = (EditText) findViewById(R.id.product_description);
+        inputProductPrice = (EditText) findViewById(R.id.product_price);
     }
 }
