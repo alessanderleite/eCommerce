bd4bfb2ace0dd68ed1ff329672ab973aabf368bb
alessanderleite <alessander.logistica@gmail.com>
2018-12-08 14:16:58 -0200

    new methods: validateProductData and storeProductInformation

--- app/src/main/java/com/example/alessander/ecommerce/AdminAddNewProductActivity.java
+++ app/src/main/java/com/example/alessander/ecommerce/AdminAddNewProductActivity.java
@@ -1,19 +1,28 @@
 package com.example.alessander.ecommerce;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class AdminAddNewProductActivity extends AppCompatActivity {
 
-    private String categoryName;
+    private String categoryName, description, price, pName, saveCurrentDate, saveCurrentTime;
     private Button addNewProductButton;
     private ImageView inputProductImage;
     private EditText inputProductName, inputProductDescription, inputProductPrice;
-
+    private static final int galleryPick = 1;
+    private Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +36,81 @@ public class AdminAddNewProductActivity extends AppCompatActivity {
         inputProductName = (EditText) findViewById(R.id.product_name);
         inputProductDescription = (EditText) findViewById(R.id.product_description);
         inputProductPrice = (EditText) findViewById(R.id.product_price);
+
+        inputProductImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                openGallery();
+            }
+        });
+
+        addNewProductButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                validateProductData();
+            }
+        });
+    }
+
+    private void openGallery() {
+
+        Intent galleryIntent = new Intent();
+        galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent, galleryPick);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == galleryPick && resultCode == RESULT_OK && data != null) {
+
+            imageUri = data.getData();
+            inputProductImage.setImageURI(imageUri);
+        }
+    }
+
+    private void validateProductData() {
+
+        description = inputProductDescription.getText().toString();
+        price = inputProductPrice.getText().toString();
+        pName = inputProductName.getText().toString();
+
+        if (imageUri == null) {
+
+            Toast.makeText(this, "Product image is mandatory...", Toast.LENGTH_SHORT).show();
+        }
+        else if (TextUtils.isEmpty(description)) {
+
+            Toast.makeText(this, "Please write product description...", Toast.LENGTH_SHORT).show();
+        }
+        else if (TextUtils.isEmpty(price)) {
+
+            Toast.makeText(this, "Please write product price...", Toast.LENGTH_SHORT).show();
+        }
+        else if (TextUtils.isEmpty(pName)) {
+
+            Toast.makeText(this, "Please write product name...", Toast.LENGTH_SHORT).show();
+        }
+        else {
+
+            storeProductInformation();
+        }
+    }
+
+    private void storeProductInformation() {
+
+        Calendar calendar = Calendar.getInstance();
+
+        SimpleDateFormat currentDate = new SimpleDateFormat("MMM dd, yyyy");
+        saveCurrentDate = currentDate.format(calendar.getTime());
+
+        SimpleDateFormat currentTime = new SimpleDateFormat("HH:mm:ss a");
+        saveCurrentTime = currentTime.format(calendar.getTime());
+
+        
     }
 }
