115be27b9646e22e16bae9a7bfaa121fb14bd459
alessanderleite <alessander.logistica@gmail.com>
2018-12-07 22:34:25 -0200

    call getIntent and pass Toast message

--- app/src/main/java/com/example/alessander/ecommerce/AdminAddNewProductActivity.java
+++ app/src/main/java/com/example/alessander/ecommerce/AdminAddNewProductActivity.java
@@ -6,11 +6,15 @@ import android.widget.Toast;
 
 public class AdminAddNewProductActivity extends AppCompatActivity {
 
+    private String categoryName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_add_new_product);
 
-        Toast.makeText(this,"Welcome Admin...", Toast.LENGTH_SHORT).show();
+        categoryName = getIntent().getExtras().get("category").toString();
+
+        Toast.makeText(this, categoryName, Toast.LENGTH_SHORT).show();
     }
 }
