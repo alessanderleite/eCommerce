a470d700ad57cb195f18c78cb9f3fafd67f6e417
alessanderleite <alessander.logistica@gmail.com>
2018-12-09 10:27:23 -0200

    change Users from private to public into Prevalent class

--- app/src/main/java/com/example/alessander/ecommerce/prevalent/Prevalent.java
+++ app/src/main/java/com/example/alessander/ecommerce/prevalent/Prevalent.java
@@ -4,7 +4,7 @@ import com.example.alessander.ecommerce.model.Users;
 
 public class Prevalent {
 
-    private static Users currentOnlineUser;
+    public static Users currentOnlineUser;
 
     public static final String userPhoneKey = "UserPhone";
     public static final String userPasswordKey = "UserPassword";
